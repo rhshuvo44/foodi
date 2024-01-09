9d69f08cd8d85933b92c96b1669ec165f65a1bc7
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-01-09 10:32:33 +0600

    react router setup

---
+++ src/layout/Main.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default Main;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#39db4a",
+        red: "#ff6868",
+        secondary: "#555",
+        primary: "#fcfcfc",
+      },
+    },
   },
   plugins: [],
-}
+};
